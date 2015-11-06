bd5a64f452ed2912004b19de6e274b8d980b4bb2
phauna <phauna@users.noreply.github.com>
2015-11-06 23:06:59 +1100

    Finished

---
+++ etch.js
@@ -0,0 +1,14 @@
+
+
+var row = function() {
+    for(var i=0; i<4; i++) {
+        var div = $('<div/>').css({'background': 'red', 'width': '50px', 'height': '50px', 'margin': '2.5px 5px', 'display': 'inline-block'}).appendTo(".container");
+    };
+    
+};
+
+row();
+row();
+row();
+row();
+
